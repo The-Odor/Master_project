bedfd4a6e86807031d0a4ed525a208d943492d86
The-Odor <TheodorMA@protonmail.com>
2024-06-10 13:30:17 +0200

    Cleanup MLAgentLearn.py
    I cry at the work I put into now-redundant functions

--- Pythonscripts/MLAgentLearn.py
+++ Pythonscripts/MLAgentLearn.py
@@ -12,16 +12,11 @@ import random
 import time
 import datetime
 import os
+from visualize import draw_net
 
 # cd C:\Users\theod\Master_project
 # venv\Scripts\activate
 # python Pythonscripts\MLAgentLearn.py
-
-# cd Master_project
-# venv\Scripts\activate
-# python
-# from Pythonscripts.MLAgentLearn import *
-# demonstrateGenome("Populations\popcount_24_simlength600_generation_101.pkl", NEAT_CONFIG)
 
 
 # TODO: make config into a file
@@ -62,50 +57,6 @@ doNothingBreakPoint = lambda : 0
 import random
 random.seed(CONFIG_DETAILS["PythonSeed"]())
 
-
-def addLayer(network, layerIdx, actFunc=nn.ReLU(), bias=True):
-    # Layer added is in the form of an identity matrix with bias=0 and should 
-    # have no effective change immediately upon addition
-    # ASSUMPTION: actFunc(actFunc(x)) = actFunc(x), like for ReLU.
-    layers = [i for i in network]
-    identitySize = network[2*layerIdx-2].out_features
-    newNetwork = nn.Sequential(*(layers[:2*layerIdx] +\
-                 [nn.Linear(identitySize, identitySize, bias=bias), actFunc] +\
-                 layers[2*layerIdx:]))
-    parameterGenerator = newNetwork.parameters()
-    next(layer for i,layer in enumerate(parameterGenerator)
-         if i==2*layerIdx).data = torch.Tensor(np.identity(identitySize))
-    next(parameterGenerator).data = torch.Tensor(np.zeros(identitySize))
-    return newNetwork
-
-    return nn.Sequential(*network, nn.Linear(inFeatures, outFeatures), actFunc)
-
-def addNode(network, layerIdx, addNNodes):
-    # TODO: Add checks like refusing to go to 0 or negative nodes
-    # Added node(s) has weights and biases of 0 and should have no effective change immediately upon addition
-    network[layerIdx*2-2].out_features += addNNodes
-    network[layerIdx*2].in_features += addNNodes
-    for i, parameters in enumerate(network.parameters()):
-        if i == layerIdx*2:
-            # Increase number of output weights in previous layer
-            updatedParameters = torch.Tensor(np.zeros(np.array(parameters.shape) + np.array((addNNodes,0)))) # .shape is annoyingly ordered as (output,input)
-            overlapShape = (min(parameters.shape[0], updatedParameters.shape[0]), min(parameters.shape[1], updatedParameters.shape[1])) # Finds overlap between old and new matrices
-            updatedParameters.data[:overlapShape[0],:overlapShape[1]] = parameters[:overlapShape[0],:overlapShape[1]]
-            parameters.data = updatedParameters
-            # Fault: slicing with [:0] apparently just give you an empty iterative :((
-            # parameters.data[:(-min(addNNodes,0)),:] = updatedParameters[:(-max(addNNodes,0)),:
-        elif i == layerIdx*2+1:
-            # Increase number of biases in relevant layer
-            updatedParameters = torch.Tensor(np.zeros(np.array(parameters.shape) + addNNodes))
-            overlapShape = min(parameters.shape[0], updatedParameters.shape[0])
-            updatedParameters[:overlapShape] = parameters[:overlapShape]
-            parameters.data = updatedParameters
-        elif i == layerIdx*2+2:
-            # Increase number if input weights in following layer
-            updatedParameters = torch.Tensor(np.zeros(np.array(parameters.shape) + np.array((0,addNNodes)))) # .shape is annoyingly ordered as (output,input)
-            overlapShape = (min(parameters.shape[0], updatedParameters.shape[0]), min(parameters.shape[1], updatedParameters.shape[1])) # Finds overlap between old and new matrices
-            updatedParameters.data[:overlapShape[0],:overlapShape[1]] = parameters[:overlapShape[0],:overlapShape[1]]
-            parameters.data = updatedParameters
 
 ### Use 1 activation function
 # activationFunc = nn.ReLU
