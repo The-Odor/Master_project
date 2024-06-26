ab8d0f89677ef945ae0ca60cbefe0ab0b5442a89
The-Odor <TheodorMA@protonmail.com>
2024-06-26 12:20:14 +0200

    Update demonstrateGenome.py
    Cleanup

--- Pythonscripts/demonstrateGenome.py
+++ Pythonscripts/demonstrateGenome.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
 
     if case == 1:
         # case 2:
+        # creates a PDF of given genome (defaults to best of latest generation)
+        learner.makePDF(genome=None)
+        
         # Demonstrates Genomes (hence name, lol)
         learner.demonstrateGenome(learner.findGeneration()[1])
         # learner.demonstrateGenome(learner.findGeneration(specificGeneration=69)[1])
+
     elif case == 2:
         # case 3:
         # Demonstrates simple motion in Unity editor
         learner.motionTest()
-    elif case == 3:
-        # case 4: 
-        # creates a PDF of given genome (defaults to best of latest generation)
-        learner.makePDF(genome=None)
