84cfbbcb788dba0cc698569fc46e4ba0c8823c4a
The-Odor <TheodorMA@protonmail.com>
2024-10-28 15:57:02 +0100

    Create FormalizedExperiment.py

---
+++ Pythonscripts/FormalizedExperiment.py
@@ -0,0 +1,22 @@
+
+
+### CTRNN, unseeded
+# Training
+pass
+# Self-evaluation
+pass
+# Other-evaluation
+
+### CTRNN, seeded
+# Training
+pass
+# Self-evaluation
+pass
+# Other-evaluation
+
+### SIN
+# Training
+pass
+# Self-evaluation
+pass
+# Other-evaluation
