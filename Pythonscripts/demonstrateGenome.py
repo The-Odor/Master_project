04e4e09897acd57db0384075c527ca3cd1a1ae05
The-Odor <TheodorMA@protonmail.com>
2025-01-09 16:06:04 +0100

    Update demonstrateGenome.py
    now takes new file format exe's being stored in subfolders  underneath main exe

--- Pythonscripts/demonstrateGenome.py
+++ Pythonscripts/demonstrateGenome.py
@@ -1,27 +1,44 @@
 from MLAgentLearn import Learner_NEAT, Learner_CMA, CONFIG_DETAILS
-
+import copy
 
 if __name__ == "__main__":
-    learnerNEAT = Learner_NEAT(CONFIG_DETAILS)
-    learnerCMA = Learner_CMA(CONFIG_DETAILS)
+    # learnerNEAT = Learner_NEAT(CONFIG_DETAILS)
+    # learnerCMA = Learner_CMA(CONFIG_DETAILS)
     # learner.demonstrateGenome(learner.findGeneration()[1])
 
     case = 1
 
     if case == 1:
         # case 1:
-        # creates a PDF of given genome (defaults to best of latest generation)
-        learnerNEAT.makePDF(genome=None)
-        
-        # Demonstrates Genomes (hence name, lol)
-        learnerNEAT.demonstrateGenome()
-        # learner.demonstrateGenome(learner.findGeneration()[1])
-        # learner.demonstrateGenome(learner.findGeneration(specificGeneration=69)[1])
+        # morphologies = ["stingray", "insect", "gecko", "babya", "spider", "queen", "tinlicker", "longleg", "salamander", "park", "squarish", "blokky", "babyb", "snake", "linkin", "ww", "turtle", "penguin", "zappa", "garrix", "ant", "pentapod"]
+        morphologies = ["salamander", "longleg", "gecko", "park", "squarish", "tinlicker"]
+        morphologies = [morph + "_v1?team=0" for morph in morphologies]
+        fullEnvironment = CONFIG_DETAILS["exeFilepath"]
+        for i,trainedMorphology in enumerate(morphologies):
+            print(f"\n\nTraining morphology {i+1} unseeded: {trainedMorphology}".upper())
+            # Training
+            # if not 'winner' in globals():  
+            learner = Learner_NEAT(copy.deepcopy(CONFIG_DETAILS), morphologyTrainedOn=[trainedMorphology])
+            tmorph = trainedMorphology[:-10]
+            index = learner.CONFIG_DETAILS["exeFilepath"].rfind(learner.CONFIG_DETAILS["unityEnvironmentName"])
+            newEnvironment = learner.CONFIG_DETAILS["exeFilepath"][:index]
+            newEnvironment+= f"{tmorph}{learner.dirSeparator}{learner.CONFIG_DETAILS['unityEnvironmentName']}{learner.CONFIG_DETAILS['fileendingFormat']}"
+            learner.switchEnvironment(newEnvironment)
+
+
+            # creates a PDF of given genome (defaults to best of latest generation)
+            learner.makePDF(genome=None)
+            
+            # Demonstrates Genomes (hence name, lol)
+            learner.demonstrateGenome()
+            # learner.demonstrateGenome(learner.findGeneration()[1])
+            # learner.demonstrateGenome(learner.findGeneration(specificGeneration=69)[1])
 
     elif case == 2:
         # case 2:
         # Demonstrates simple motion in Unity editor
-        learnerNEAT.motionTest()
+        learner = Learner_NEAT(copy.deepcopy(CONFIG_DETAILS))
+        learner.motionTest()
 
     elif case == 3:
         # case 3:
